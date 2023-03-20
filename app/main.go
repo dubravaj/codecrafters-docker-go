38d61a6f743fe0c0ff7335c38b281366d6df7598
dubravaj <juraj.dubrava@rouvy.com>
2023-03-20 19:51:53 +0100

    pass 1st stage

--- app/main.go
+++ app/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	// Uncomment this block to pass the first stage!
-	// "os"
-	// "os/exec"
+	"os"
+	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -14,15 +14,15 @@ func main() {
 
 	// Uncomment this block to pass the first stage!
 	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(output))
 }
