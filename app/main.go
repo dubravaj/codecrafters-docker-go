ca28bb29eb832bdd9694686355af8bce3024c7e0
dubravaj <juraj.dubrava@rouvy.com>
2023-03-20 20:25:06 +0100

    Child process output to parent process

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+
 	// Uncomment this block to pass the first stage!
 	"os"
 	"os/exec"
@@ -10,7 +10,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
@@ -18,11 +18,14 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
-	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	// output, err := cmd.Output()
+	// if err != nil {
+	// 	fmt.Printf("Err: %v", err)
+	// 	os.Exit(1)
+	// }
 
-	fmt.Println(string(output))
+	// fmt.Println(string(output))
 }
